0b994f48491e00863cc6491943e1f0bf9917f46c
XF4rry <folishmercenary46@gmail.com>
2025-01-02 18:47:53 +0100

    aggiunta ricerca traccia con player (controlla todo) Co-authored-by: R3DZY <R3DZY@users.noreply.github.com>

--- Backend/Routers/api.js
+++ Backend/Routers/api.js
@@ -60,20 +60,109 @@ async function getArtistIdByName(artistName, accessToken) {
     }
 }
 
+//ricerca l'id della traccia per nome
+async function  getTrackIdByName(TrackName, accessToken) {
+    const url = `https://api.spotify.com/v1/search?q=${encodeURIComponent(TrackName)}&type=track`;  //al posto di type ho provato track??
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    });
+    console.log(response);
+
+    if (!response.ok) {
+        throw new Error(`Error fetching artist: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    
+    if (data && data.tracks && data.tracks.items.length > 0) {
+        return data.tracks.items[0].id; // Restituisce l'ID della prima traccia trovata
+    } else {
+        throw new Error('No track found with that name.');
+    }
+}
+
 // Endpoint per cercare l'ID dell'artista dal nome
 router.post(`/search-artist`, async (req, res) => {
-    const artistName = req.body.artistName; // Ottieni il nome dell'artista dal corpo della richiesta
+    const search = req.body.search; // Ottieni il nome dell'artista dal corpo della richiesta
 
-    try {
+    try {                  
         const accessToken = await getAccessToken();
-        const artistId = await getArtistIdByName(artistName, accessToken);
-        res.json({ artistId });
+        const trackId= await getTrackIdByName(search, accessToken);   //cambiato il primo parametro di ricerca
+        //const artistId = await getArtistIdByName(search, accessToken);  
+        //const artistData = await getArtistData(artistId, accessToken);
+        //console.log('Artist Data:', artistData.name + " " + artistData.id + " " + artistData.genres + " " + artistData.type);
+        //res.json({ artistData });
+        const trackData= await getTrackData(trackId, accessToken);
+        res.json(trackData);
     } catch (error) {
         console.error('Error:', error.message);
         res.status(500).json({ error: error.message });
     }
 });
 
+async function getTrackData(trackId, accessToken) {
+    const urlTrackData = 'https://api.spotify.com/v1/tracks/' + trackId;   // chiamata api tramite l'id della traccia
+
+    try {
+        //const accessToken = await getAccessToken();
+
+        // Ottenere i dati della traccia    
+        const trackResponse = await fetch(urlTrackData, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!trackResponse.ok) {
+            throw new Error(`Error fetching track data: ${trackResponse.statusText}`);
+        }
+
+        const trackData = await trackResponse.json();
+        console.log('Track Data:', trackData);
+        return(trackData);    //stampa dei dati della traccia
+
+    } catch (error) {
+        console.error('Error:', error.message);
+        return(error.message);
+    }
+}
+
+
+async function getArtistData(artistId, accessToken) {
+    const urlArtitsData = 'https://api.spotify.com/v1/artists/' + artistId;   // chiamata api tramite l'id dell'artista
+
+    try {
+        //const accessToken = await getAccessToken();
+
+        // Ottenere i dati dell'artista
+        const artistResponse = await fetch(urlArtitsData, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!artistResponse.ok) {
+            throw new Error(`Error fetching artist data: ${artistResponse.statusText}`);
+        }
+
+        const artistData = await artistResponse.json();
+        console.log('Artist Data:', artistData);
+        return(artistData);    //stampa dei dati dell'artista 
+
+    } catch (error) {
+        console.error('Error:', error.message);
+        return(error.message);
+    }
+}
 // Endpoint originale per ottenere i dati dell'artista
 /*router.post(`/kanye`, async (req, res) => {
     const urlArtitsData = 'https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg';
--- Frontend/index.js
+++ Frontend/index.js
@@ -28,13 +28,41 @@ $(document).ready(function() {
             return response.json(); // Converte la risposta in JSON
         })
         .then(data => {
+            console.log('Dati dell\'artista:', data);
+            console.log('ID dell\'artista:', data.id);
+            console.log('URL dell\'embed:' + `https://open.spotify.com/embed/${data.type}/${data.id}?utm_source=generator&theme=0`)
+
+            //console.log('vaffanculo');
             // Costruisci l'HTML per aggiornare il contenitore con i dati dell'artista
-            const htmlContent = `
-                <p><strong>Nome:</strong> ${data.artistName}</p>
-                <p><strong>ID:</strong> ${data.artistId}</p>
-            `;
+            //per id dell'artista <p><strong>ID:</strong> ${dati.id}</p>
             
+            /*const htmlContent = `
+                <p><strong>Nome:</strong> ${dati.name}</p>   
+                <p><strong>Genere:</strong> ${dati.genres}</p>
+                <p><strong>Tipo:</strong> ${dati.type}</p>
+                <img src="${dati.images[0].url}" alt="Immagine dell'artista">
+                <style>
+                    #responseContainer img {
+                        width: 200px;
+                        border-radius: 50%;
+                        margin: 20px auto;
+                    }
+                </style>`;*/
+            
+
+         
+            const htmlContent = `<iframe
+        title="Spotify Embed: Recommendation Playlist "
+        src='https://open.spotify.com/embed/${data.type}/${data.id}?utm_source=generator&theme=0'
+        width="100%"
+        height="100%"
+        style="border-radius: 12px; overflow: hidden;"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        >`
             // Aggiorna solo la parte della pagina desiderata
+            
             document.querySelector('#responseContainer').innerHTML = htmlContent;
         })
         .catch(error => {
