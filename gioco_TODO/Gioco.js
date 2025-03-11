36e03e100859b103401726e529407c8512b1152b
XF4rry <christian.pastori.stud@ispascalcomandini.it>
2025-03-11 12:35:59 +0100

    sistemato

--- gioco_TODO/Gioco.js
+++ gioco_TODO/Gioco.js
@@ -2,6 +2,38 @@ const express = require('express');
 const app = express();
 const port = 3002;
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const appFirebase = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//aggiungi ad una collection
+import { collection, addDoc } from "firebase/firestore";
+
+async function createUser(db, nuovoPunteggio) {
+  try {
+    const docRef = await addDoc(collection(db, "classifica"), nuovoPunteggio);
+    console.log("punteggio aggiunto: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding points: ", e);
+  }
+}
+
+// Example usage:
+//const newUser = { name: "Alice", email: "alice@example.com" };
+//createUser(db, newUser);
+
+//legge da una collection
+async function getUsers(db) {
+  const querySnapshot = await getDocs(collection(db, "classifica"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+  });
+}
+
+//getUsers(db);
+
 
 app.use(express.json());
 
---
+++ gioco_TODO/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBPYEFH9F6JwguI4DGTTXXDl0JA6cJZ4mQ",
+    authDomain: "spotydma.web.app",
+    projectId: "spotydma",
+    storageBucket: "spotydma.firebasestorage.app",
+    messagingSenderId: "833117624421",
+    appId: "1:833117624421:web:e66b3fe3251d82ecb5ce8c",
+    measurementId: "G-H2B0CFKM4Y"
+  };
